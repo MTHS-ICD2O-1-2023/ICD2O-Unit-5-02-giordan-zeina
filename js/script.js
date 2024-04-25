f4f2de83bed48d5b2108e71f2490c79b0a55a5d9
Giordan Zeina <144264315+giordan-zeina@users.noreply.github.com>
2024-04-25 13:33:25 +0000

    favicon and code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Giordan Zeina. All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Giordan Zeina
+// Created on: April 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+
+function myButtonClicked() {
+  const positiveButton = document.getElementById("positive").checked
+
+  if (positiveButton == true) {
+    const randomNumber = Math.floor(Math.random() * 6) + 1
+    document.getElementById("answer").innerHTML =
+      "Your number is: " + randomNumber
+  } else {
+    const randomNegativeNumber = Math.floor(Math.random() * 6 + 1) * -1
+    document.getElementById("answer").innerHTML =
+      "Your number is: " + randomNegativeNumber
+  }
+}
